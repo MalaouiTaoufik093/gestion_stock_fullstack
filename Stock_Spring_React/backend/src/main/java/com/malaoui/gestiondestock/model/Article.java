fad1a4ce35ba816832cfdc8a5bf8d3b83034aad2
Taoufik <malaouitaoufik1@gmail.com>
2021-12-05 16:22:13 +0100

    05_12_2021_16_22

--- Stock_Spring_React/backend/src/main/java/com/malaoui/gestiondestock/model/Article.java
+++ Stock_Spring_React/backend/src/main/java/com/malaoui/gestiondestock/model/Article.java
@@ -1,5 +1,8 @@
 package com.malaoui.gestiondestock.model;
 
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
@@ -16,5 +19,23 @@ public class Article extends AbstractEntity {
 	
 	@Id
 	private Integer id;
-
+	
+	@Column(name="codearticle")
+	private String codeArticle;
+	 
+	@Column(name="designation")
+	private String designtion;
+	
+	@Column(name="prixunitaireht")
+	private BigDecimal prixUnitaireHt;
+	
+	@Column(name="tauxtva")
+	private BigDecimal tauxTva;
+	
+	@Column(name="prixunitairettc")
+	private BigDecimal prixUnitaireTtc;
+	
+	@Column(name="photo")
+	private String photo;
+	
 }
